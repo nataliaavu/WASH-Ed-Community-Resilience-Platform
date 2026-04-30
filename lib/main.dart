7d38af2ae0fd63612edb4531019b734b2daaa5b9
nataliaavu <nataliavu2307@gmail.com>
2026-04-30 22:59:11 +1000

    feat: adding nav bar to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'widgets/navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'WASH-Ed Community Resilience'),
+      home: const AppNavigationBar(),
     );
   }
 }
